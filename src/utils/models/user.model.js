33c8fc5d01758bb1df3c5c9ee3ca50bb4c15258e
jlukasiu <jedrzej.lukasiuk@gmail.com>
2018-05-25 23:46:53 +0200

    Added user model

---
+++ src/utils/models/user.model.js
@@ -0,0 +1,23 @@
+export const userModel = {
+  id: 0,
+  name: "",
+  username: "",
+  email: "",
+  address: {
+    street: "",
+    suite: "",
+    city: "",
+    zipcode: "",
+    geo: {
+      lat: "",
+      lng: ""
+    }
+  },
+  phone: "",
+  website: "",
+  company: {
+    name: "",
+    catchPhrase: "",
+    bs: ""
+  }
+}
